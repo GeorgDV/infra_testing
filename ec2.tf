23ec722ac834e808f5c544f6926851d6fe03fb66
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-02-05 14:43:35 +0200

    Add key_name for ec2

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-07ebfd5b3428b6f4d" # ubuntu
   instance_type = "t2.micro"
 
+  key_name = "infra_test_keypair"
+
   tags = {
     Name = "My Super Tuper Awesome Learning Machine_!!!"
   }
