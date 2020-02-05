f8a948427173dbc92ddbb321d0c03f806f3b85c5
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-02-05 14:35:56 +0200

    Fix my machine and life

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-07ebfd5b3428b6f4d" # ubuntu
-  instance_type = "t3.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Hello_World_!!"
